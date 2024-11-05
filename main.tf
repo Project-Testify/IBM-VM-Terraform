474435f504b793e6ef850a0fb99db18c01b47862
nsavinda <nirmalsavinda29@gmail.com>
2024-11-05 23:32:24 +0530

    feat: internet rules

--- main.tf
+++ main.tf
@@ -1,32 +1,42 @@
 data "ibm_resource_group" "group" {
-  name = "Test"
+  name = "Testify"
 }
 
 resource "ibm_is_vpc" "vpc" {
-    name = "test-vpc"
+    name = "testify-vpc"
     resource_group = data.ibm_resource_group.group.id
+
+}
+
+resource "ibm_is_public_gateway" "pgw" {
+  name = "public-gateway"
+  vpc  = ibm_is_vpc.vpc.id
+  zone = "jp-osa-1"
+  resource_group = data.ibm_resource_group.group.id
 
 }
 
 resource "ibm_is_subnet" "subnet" {
-    name = "test-subnet"
+    name = "testify-subnet"
     vpc = ibm_is_vpc.vpc.id
     zone = "jp-osa-1"
     ipv4_cidr_block = "10.248.0.0/18"
     resource_group = data.ibm_resource_group.group.id
+    public_gateway = ibm_is_public_gateway.pgw.id
+
 
   
 }
 
 resource "ibm_is_floating_ip" "public_ip" {
-  name           = "test-vm-ip"
+  name           = "testify-vm-ip"
   target         = ibm_is_instance.vm_instance.primary_network_interface[0].id
   resource_group = data.ibm_resource_group.group.id
 }
 
 resource "ibm_is_instance" "vm_instance" {
-  name    = "test-vm"
-  image   = "r034-32af8cbf-f7c7-438f-8b0e-e8ecbdba0b00"
+  name    = "testify-vm"
+  image   = "r034-2aee2fde-574f-46a1-be77-e919b6847a9f"
   profile = "bx2-2x8"
 
   primary_network_interface {
@@ -67,20 +77,36 @@ resource "ibm_is_security_group_rule" "ssh_access" {
   
 }
 
+resource "ibm_is_security_group_rule" "outbound_internet" {
+  group     = ibm_is_security_group.sg.id
+  direction = "outbound"
+  ip_version = "ipv4"
+  remote     = "0.0.0.0/0"
+}
+
+# Allow 80 443
+resource "ibm_is_security_group_rule" "http_access" {
+  group     = ibm_is_security_group.sg.id
+  direction = "inbound"
+  ip_version = "ipv4"
+  remote     = "0.0.0.0/0"
+
+}
+
 
 # connect to the instance using the floating IP and Run init.sh
 
-resource "null_resource" "init" {
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "root"
-      private_key = file("~/.ssh/id_rsa")
-      host        = ibm_is_floating_ip.public_ip.address
-    }
+# resource "null_resource" "init" {
+#   provisioner "remote-exec" {
+#     connection {
+#       type        = "ssh"
+#       user        = "root"
+#       private_key = file("~/.ssh/id_rsa")
+#       host        = ibm_is_floating_ip.public_ip.address
+#     }
 
-    script = "init.sh"
-  }
+#     script = "init.sh"
+#   }
 
-  depends_on = [ibm_is_floating_ip.public_ip]
-}
+#   depends_on = [ibm_is_floating_ip.public_ip]
+# }
